e05ad01d745432e36b87562fb040e2b7b7643305
poweiiiii <4b0g0175@stust.edu.tw>
2024-05-02 16:41:18 +0800

    first commit

---
+++ src/assets/Myslider.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useState } from 'react';
+
+const Myslider = () => {
+  return (
+    <div>
+      <input type=''></input>
+    </div>
+  )
+}
+
+export default Myslider
